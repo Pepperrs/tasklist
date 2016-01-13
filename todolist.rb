43c9bf7a8fe425cdf05d1ea362ea0dca4185d2e2
Peter Schüllermann <Peter.schuellermann@gmail.com>
2016-01-13 07:55:06 +0100

    move add_item to TodoList add rename fo TodoList

--- todolist.rb
+++ todolist.rb
@@ -1,10 +1,21 @@
 class TodoList
   attr_reader :title, :items
+
   # methods and stuff go here
   # Initialize todo list with a title and no items
   def initialize(list_title)
     @title = list_title
     @items = [] # Starts empty! No Items yet!
+  end
+
+  def rename(new_name)
+    @title = new_name
+  end
+
+  # Creates a new Item and adds it to the array of Items
+  def add_item(new_item)
+    item = Item.new(new_item)
+    @items.push(item)
   end
 end
 
@@ -14,11 +25,4 @@ class Item
     @description = item_description
     @completed_status = false
   end
-
-  # Creates a new Item and adds it to the array of Items
-  def add_item(new_item)
-    item = Item.new(new_item)
-    @items.push(item)
-  end
-
 end
